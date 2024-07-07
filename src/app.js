d52106a68202df641c2af281ba2ef99c76a8312b
Shekhar3742 <imderek3742@gmail.com>
2024-07-07 21:20:04 +0530

    route setup lecTwelve

--- src/app.js
+++ src/app.js
@@ -14,4 +14,11 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true,  limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// import router
+import userRouter from "./routes/user.js"
+
+// routes deceleration
+app.use("/api/v1/users", userRouter) //aise karne se app.js ganda nhi hoga user me jane ke bad control user router ke pass jayega phir waha se jaha route karna h kar sakte h 
+
 export { app }
